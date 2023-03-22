2e74ea21cb64ed16ca99d395ed5e73584203675c
Kelly Tan <kellytan.2019@scis.smu.edu.sg>
2023-03-22 13:40:58 +0800

    less distance calc

--- trajectory_buffer/trajectory_buffer_final.py
+++ trajectory_buffer/trajectory_buffer_final.py
@@ -4,7 +4,7 @@ import ot
 #for code checking
 import pandas as pd
 
-class TrajectoryBuffer(object):
+class TrajectoryBuffer():
     '''
         For storing trajectories. Stores only trajectories from the latest run for each level.
     '''
@@ -66,75 +66,122 @@ class TrajectoryBuffer(object):
                 sampled_timesteps = np.random.randint(action_obs_samples.shape[0],size=2000)
                 action_obs_samples = action_obs_samples[sampled_timesteps, :]
             self.processed_trajs[seed] = action_obs_samples
-            
     
-    def _distance_working_buffer(self, level_sampler):
-        working_seeds = list(level_sampler.working_seed_set)
-        working_seeds.sort()
 
-        distance_matrix = np.zeros(shape=(len(working_seeds),len(working_seeds)))
-        distance_matrix[:,:] = np.nan
+    # def _distance_working_buffer(self, level_sampler):
+    #     #calculate distances only for last 5 seeds in staleness + regret ranking
 
-        i = 0
+    #     #get last ranking 5 seeds
+    #     weights = level_sampler.sample_weights()
+    #     num_rank = 5
+    #     lowest_seeds = level_sampler.seeds[np.argsort(weights)<num_rank]
 
-        for working_seed_idx in range(len(working_seeds)):
-            working_seed = working_seeds[working_seed_idx]
-            working_traj = self.processed_trajs[working_seed]
+    #     working_seeds = list(lowest_seeds)
+    #     working_seeds.sort()
 
-            for compare_seed_idx in range(working_seed_idx+1, len(working_seeds)):
-                compare_seed = working_seeds[compare_seed_idx]
-                compare_traj = self.processed_trajs[compare_seed]
+    #     # working_seeds = list(level_sampler.working_seed_set)
+    #     # working_seeds.sort()
 
-                s_working = [1/working_traj.shape[0]]*working_traj.shape[0]
-                s_compare = [1/compare_traj.shape[0]]*compare_traj.shape[0]
-                M = ot.dist(working_traj, compare_traj, metric='euclidean')
-                W = ot.emd2(s_working, s_compare, M, numItermax=100000)
-                distance_matrix[working_seed_idx][compare_seed_idx] = W
-                distance_matrix[compare_seed_idx][working_seed_idx] = W
+    #     distance_matrix = np.zeros(shape=(len(working_seeds),len(working_seeds)))
+    #     distance_matrix[:,:] = np.nan
 
-                i+=1
+    #     i = 0
 
-        print(i)
+    #     for working_seed_idx in range(len(working_seeds)):
+    #         working_seed = working_seeds[working_seed_idx]
+    #         working_traj = self.processed_trajs[working_seed]
 
-        print(distance_matrix)
+    #         for compare_seed_idx in range(working_seed_idx+1, len(working_seeds)):
+    #             compare_seed = working_seeds[compare_seed_idx]
+    #             compare_traj = self.processed_trajs[compare_seed]
 
-        for seed_idx in range(len(working_seeds)):
-            seed = working_seeds[seed_idx]
-            distance = np.nanmin(distance_matrix[seed_idx])
-            level_sampler.update_diversity(seed, distance)
+    #             s_working = [1/working_traj.shape[0]]*working_traj.shape[0]
+    #             s_compare = [1/compare_traj.shape[0]]*compare_traj.shape[0]
+    #             M = ot.dist(working_traj, compare_traj, metric='euclidean')
+    #             W = ot.emd2(s_working, s_compare, M, numItermax=100000)
+    #             distance_matrix[working_seed_idx][compare_seed_idx] = W
+    #             distance_matrix[compare_seed_idx][working_seed_idx] = W
+
+    #             # i+=1
+
+    #     # print(i)
+
+    #     # print(distance_matrix)
+
+    #     for seed_idx in range(len(working_seeds)):
+    #         seed = working_seeds[seed_idx]
+    #         distance = np.nanmin(distance_matrix[seed_idx])
+    #         level_sampler.update_diversity(seed, distance)
 
 
-    def _distance_staging_buffer(self, level_sampler):
-        staging_seeds = list(level_sampler.staging_seed_set)
-        working_seeds = list(level_sampler.working_seed_set)
+    # def _distance_staging_buffer(self, level_sampler):
+    #     staging_seeds = list(level_sampler.staging_seed_set)
+    #     working_seeds = list(level_sampler.working_seed_set)
 
-        i = 0
-        for staging_seed in staging_seeds:
-            staging_traj = self.processed_trajs[staging_seed]
-            distances = np.zeros(shape=len(working_seeds))-1
-            for working_idx in range(len(working_seeds)):
-                working_seed = working_seeds[working_idx]
+    #     i = 0
+    #     for staging_seed in staging_seeds:
+    #         staging_traj = self.processed_trajs[staging_seed]
+    #         distances = np.zeros(shape=len(working_seeds))-1
+    #         for working_idx in range(len(working_seeds)):
+    #             working_seed = working_seeds[working_idx]
+    #             working_traj = self.processed_trajs[working_seed]
+
+    #             s_staging = [1/staging_traj.shape[0]]*staging_traj.shape[0]
+    #             s_working = [1/working_traj.shape[0]]*working_traj.shape[0]
+    #             M = ot.dist(staging_traj, working_traj, metric='euclidean')
+    #             W = ot.emd2(s_staging, s_working, M, numItermax=100000)
+    #             distances[working_idx] = W
+
+    #             i+=1
+    #         level_sampler.update_diversity(staging_seed, np.min(distances))
+
+    #     # print(i)
+
+    def _distance(self, eval_seeds, highest_seeds):
+        for eval_seed in eval_seeds:
+            eval_traj = self.processed_trajs[eval_seed]
+            distances = np.zeros(shape=len(highest_seeds))-1
+            for working_idx in range(len(highest_seeds)):
+                working_seed = highest_seeds[working_idx]
                 working_traj = self.processed_trajs[working_seed]
 
-                s_staging = [1/staging_traj.shape[0]]*staging_traj.shape[0]
-                s_working = [1/working_traj.shape[0]]*working_traj.shape[0]
-                M = ot.dist(staging_traj, working_traj, metric='euclidean')
+                #empty list means uniform distribution
+                s_staging = []
+                s_working = []
+                M = ot.dist(eval_traj, working_traj, metric='euclidean')
                 W = ot.emd2(s_staging, s_working, M, numItermax=100000)
                 distances[working_idx] = W
 
-                i+=1
-            level_sampler.update_diversity(staging_seed, np.min(distances))
-
-        print(i)
+            level_sampler.update_diversity(eval_seed, np.min(distances))
 
     def calculate_wasserstein_distance(self, level_sampler, discard_grad):
+
         #in first iteration diversity score is 0
         if len(level_sampler.working_seed_set)==0:
             for seed in level_sampler.staging_seed_set:
                 level_sampler.update_diversity(seed, 0)
             return
 
+        # if discard_grad:
+        #     self._distance_staging_buffer(level_sampler)
+        # else:
+        #     self._distance_working_buffer(level_sampler)
+    
+        #get highest ranking 5 seeds
+        #rmbr to change sample weights accordingly
+        weights = level_sampler.sample_weights()
+        seen_seeds = level_sampler.seeds[level_sampler.unseen_seed_weights==0]
+        seen_weights = weights[level_sampler.unseen_seed_weights==0]
+
+        num_rank = 5
+        highest_seeds = list(seen_seeds[np.argsort(seen_weights)>=len(seen_weights)-num_rank])
+
         if discard_grad:
-            self._distance_staging_buffer(level_sampler)
+            eval_seeds = list(level_sampler.staging_seed_set)
         else:
-            self._distance_working_buffer(level_sampler)
+            lowest_seeds = seen_seeds[np.argsort(seen_weights)<num_rank]
+            eval_seeds = list(lowest_seeds)
+
+        print(len(eval_seeds), len(highest_seeds))
+        print(eval_seeds, highest_seeds)
+        self._distance(eval_seeds, highest_seeds)
